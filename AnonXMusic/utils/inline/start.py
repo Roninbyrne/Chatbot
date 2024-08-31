f0cfe684842d49ccb7ace1c350590f56c31dc65e
Ronin <bhabyaprakash01@gmail.com>
2024-08-31 10:51:19 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -9,18 +9,6 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
-
-
-def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
@@ -29,9 +17,11 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-    ]
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="digerbotlar"),
         [
-                    [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="digerbotlar"),
         ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        ],
+    ]
     return buttons
